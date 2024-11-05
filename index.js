e1f2a7de1dd9e7a8ee9b9f308a33b4a2e25e0917
Ahmed Al-3adl <ahmedmohamedal3adl@gmail.com>
2024-11-05 10:13:30 +0200

    Adding the frontend part

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
   return [
