0f31d14045b9ba38a09b9e46dd129f75a8e009a1
Ahmed Al-3adl <ahmedmohamedal3adl@gmail.com>
2024-11-06 17:11:48 +0200

    Finishing exercises of phonebook until: 3.21

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-// app.use(cors())
+const Person = require('./models/person')
+
+app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
@@ -38,46 +41,90 @@ let phonebook = [
     }
 ]
 
-app.get('/api/persons', (request, response)=>
+app.get('/api/persons', (request, response, next)=>
 {
-    return response.json(phonebook).end()
+    Person.find({})
+      .then((persons)=> response.json(persons))
+      .catch((err)=> next(err))
 })
 
-app.get('/info', (request, response)=>
+app.get('/info', (request, response, next)=>
 {
-    return response.send(`<div>
-                            <p>Phonebook has info for ${phonebook.length} people</p>
-                            <p>${new Date()}</p>
-                        <div>`)
+    Person.countDocuments()
+      .then((result)=>
+      {
+        return response.send(`<div>
+                                <p>Phonebook has info for ${result} people</p>
+                                <p>${new Date()}</p>
+                              <div>`)
+      })
+      .catch((err)=> next(err))
 })
 
-app.get('/api/persons/:id', (request, response)=>
+app.get('/api/persons/:id', (request, response, next)=>
 {
     const id = request.params.id
-    const person = phonebook.find(personP=>personP.id === id)
-    if(!person) return response.status(404).end()
-    response.json(person)
+    Person.findById(id)
+      .then(person => {
+        return response.json(person)
+      })
+      .catch((err)=> next(err))
+      
 })
 
-app.delete('/api/persons/:id', (request, response)=>
+app.delete('/api/persons/:id', (request, response, next)=>
 {
     const id = request.params.id
-    phonebook = phonebook.filter(person=> person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndDelete(id)
+      .then(()=> response.status(204).end())
+      .catch((err)=> next(err))
 })
 
-app.post('/api/persons', (request, response)=>
+app.post('/api/persons', (request, response, next)=>
 {
-    const id = Math.trunc(Math.random() * 1000000000)
     const {name, number} = request.body
     if(!name) return response.status(400).json({error: "name is required"}).end()
     if(!number) return response.status(400).json({error: "number is required"}).end()
     const person = phonebook.find(person=>person.name === name)
     if(person) return response.status(400).json({error: "name must be unique"}).end()
 
-    const newPerson = {id, name, number}
-    phonebook = phonebook.concat(newPerson)
-    response.json(newPerson).end()
+    const newPerson = new Person({name, number})
+    newPerson.save()
+      .then((newOne)=> response.json(newOne))
+      .catch((err)=> next(err))
+    // response.json(newPerson).end()
 })
-const PORT = 3001
+
+app.put('/api/persons/:id', (request, response, next)=>
+{
+    const {name, number} = request.body
+    const id = request.params.id
+    const newPersonData = {name, number}
+    Person.findByIdAndUpdate(id, newPersonData, {new: true, runValidators: true, context:'query'})
+      .then((modifiedPerson)=>
+      {
+        return response.json(modifiedPerson)
+      })
+      .catch((err)=>next(err))
+})
+const errHandler = (err, request, response, next)=>
+{
+    const message = err.message
+    const name = err.name
+    console.log({name, message})
+    
+    switch(name)
+    {
+      case "CastError":
+        response.stats(400).json({error: "Malformatted id"}).end()
+        break
+      case 'ValidationError':
+        response.status(400).json({error: message}).end()
+        break
+      default: response.status(500).json({name, message})
+    }
+}
+
+app.use(errHandler)
+const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=> console.log(`Server is running... on port ${PORT}`))
